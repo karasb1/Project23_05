821d1099fe0bb7ac6e53c1576445cd200e424dad
karasb1 <bohdankaras456@gmail.com>
2024-05-23 18:55:40 +0300

    /

--- main.cpp
+++ main.cpp
@@ -2,9 +2,42 @@
 #include <ctime>
 using namespace std;
 
-int main()
-{
+int main() {
+    srand(time(0));
+    const int FULL_SIZE = 20;
+    int arr[FULL_SIZE];
+    int size;
+    do
+    {
+        cout << "Enter the size of the array: ";
+        cin >> size;
+    } while (size > FULL_SIZE || size <= 0);
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 10 - 10;
+    }
+    do
+    {
+        cout << "Array: ";
+        for (size_t i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
 
 
-	return 0;
+
+
+
+
+
+
+
+
+
+        break;
+    } while (true);
+
+
+
+    return 0;
 }
