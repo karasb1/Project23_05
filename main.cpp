4177abd8a52b1b3ea3fccb8a5b3883b9b910e8ad
karasb1 <bohdankaras456@gmail.com>
2024-05-23 19:21:40 +0300

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,54 @@ using namespace std;
 
 int main()
 {
+    srand(time(0));
+    int arr1[10];
+    int arr2[5];
+    int arr3[5];
+    int k = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        arr1[i] = rand() % 20;
+    }
+    do
+    {
+        cout << "Array1: ";
+        for (size_t i = 0; i < 10; i++)
+        {
+            cout << arr1[i] << " ";
+        }
+        cout << endl;
+        cout << "Array2: ";
+        for (size_t i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0) {
+                arr2[k] = arr1[i];
+                k++;
+            }
+        }
+        for (size_t i = 0; i < 5; i++)
+        {
+            cout << arr2[i] << " ";
+        }
+        k = 0;
+        cout << endl;
+        cout << "Array3: ";
+        for (size_t i = 0; i < 10; i++)
+        {
+            if (i % 2 != 0) {
+                arr3[k] = arr1[i];
+                k++;
+            }
+        }
+        for (size_t i = 0; i < 5; i++)
+        {
+            cout << arr3[i] << " ";
+        }
+        cout << endl;
 
 
-	return 0;
+        break;
+    } while (true);
+
+    return 0;
 }
