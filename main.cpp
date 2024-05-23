5df0fb7f352cc3260c11c6c4e5dbacf3a0dcae92
karasb1 <bohdankaras456@gmail.com>
2024-05-23 20:19:16 +0300

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -3,41 +3,29 @@
 using namespace std;
 
 int main() {
-    srand(time(0));
-    const int FULL_SIZE = 20;
-    int arr[FULL_SIZE];
-    int size;
-    do
-    {
-        cout << "Enter the size of the array: ";
-        cin >> size;
-    } while (size > FULL_SIZE || size <= 0);
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = rand() % 10 - 10;
-    }
-    do
-    {
-        cout << "Array: ";
-        for (size_t i = 0; i < size; i++)
-        {
-            cout << arr[i] << " ";
-        }
+	srand(time(0));
+	const int size = 20;
+	int arr[size];
 
+	int k = 0;
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 10 - 5;
+	}
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl << endl;
+	for (int i = 0; i < size; ++i) {
+		if (arr[i] != 0) {
+			arr[k++] = arr[i];
+		}
+	}
+	for (int i = k; i < size; ++i) {
+		arr[i] = -1;
+	}
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
 
-
-
-
-
-
-
-
-
-
-        break;
-    } while (true);
-
-
-
-    return 0;
+	return 0;
 }
